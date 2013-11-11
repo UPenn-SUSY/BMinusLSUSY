4a4b279c71c2890376975238f81f799bbc722f22
bdjackson <bjack3@gmail.com>
2013-11-11 22:43:45 +0100

    small changes to LheToPool.sh

--- SampleProcess/LheToPool.sh
+++ SampleProcess/LheToPool.sh
@@ -6,16 +6,16 @@ OUT_FILE_SHORT="SampleProcess"
 FORMATED_LHE_NAME="${OUT_FILE_SHORT}_unweighted_events.lhe._000001.events"
 
 # move lhe file to convenient location & untar
-ln -s ${IN_FILE} ${FORMATED_LHE_NAME}.tar.gz
-tar -cvzf ${FORMATED_LHE_NAME}.tar.gz ${FORMATED_LHE_NAME}
+cp ${IN_FILE} ${FORMATED_LHE_NAME}.tar.gz
+echo tar -cvzf ${FORMATED_LHE_NAME}.tar.gz ${FORMATED_LHE_NAME}
 
-# get set up
-# TODO move this to another script
-asetup AtlasProduction,17.2.8.3
-export JOBOPTSEARCHPATH=/cvmfs/atlas.cern.ch/repo/sw/Generators/MC12JobOptions/latest/common:$JOBOPTSEARCHPATH
-
+# # get set up
+# # TODO move this to another script
+# asetup AtlasProduction,17.2.8.3
+# export JOBOPTSEARCHPATH=/cvmfs/atlas.cern.ch/repo/sw/Generators/MC12JobOptions/latest/common:$JOBOPTSEARCHPATH
+# 
 # generate 
-Generate_trf.py ecmEnergy=8000 \
+echo Generate_trf.py ecmEnergy=8000 \
                 runNumber=1 \
                 firstEvent=1 \
                 maxEvents=-1 \
@@ -24,5 +24,5 @@ Generate_trf.py ecmEnergy=8000 \
                 outputEVNTFile=evgen.${OUT_FILE_SHORT}_pool.root  \
                 inputGeneratorFile=${FORMATED_LHE_NAME}.tar.gz 
 
-mv Generate.log ${OUT_FILE_SHORT}.log
+# mv Generate.log ${OUT_FILE_SHORT}.log
 
