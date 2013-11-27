8f0b5ba76b43a5b086a1fca392de22592e8137b1
bdjackson <bjack3@gmail.com>
2013-11-27 20:47:07 +0100

    rename files

--- BMinusLTestSamples/RunBMinusLTestSamples.sh
+++ BMinusLTestSamples/RunBMinusLTestSamples.sh
@@ -8,18 +8,19 @@
 
 
 for mass in 100 500 1000; do
+  model_dir="BMinusLTestSamples"
   model_name="BMinusLTestSamples_$mass"
 
   # generate diagrams and set up MG5 area
   echo "Generating diagrams in MadGraph"
-  ./bin/mg5 ../${model_name}/Configure${model_name}.sh
+  ./bin/mg5 ../${model_dir}/Configure${model_name}.sh
 
   # copy relevant files to Cards directory
   echo "Copying files to Cards directory"
-  cp ../${model_name}/${model_name}.slha    ${model_name}/Cards/param_card.dat
-  cp ../${model_name}/pythia_card.dat       ${model_name}/Cards/pythia_card.dat
-  cp ../${model_name}/run_card.dat          ${model_name}/Cards/run_card.dat
-  cp ../${model_name}/me5_configuration.txt ${model_name}/Cards/me5_configuration.txt
+  cp ../${model_dir}/${model_name}.slha    ${model_name}/Cards/param_card.dat
+  cp ../${model_dir}/pythia_card.dat       ${model_name}/Cards/pythia_card.dat
+  cp ../${model_dir}/run_card.dat          ${model_name}/Cards/run_card.dat
+  cp ../${model_dir}/me5_configuration.txt ${model_name}/Cards/me5_configuration.txt
 
   # move to model workspace
   cd ${model_name}
