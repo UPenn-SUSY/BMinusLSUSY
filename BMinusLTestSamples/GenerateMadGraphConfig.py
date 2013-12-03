99e46588211c7bad4a8a6f81f1434ea742463c20
bdjackson <bjack3@gmail.com>
2013-12-04 00:30:58 +0100

    update RunBMinusLTestSamples.sh script and run_card_wojets.dat

--- BMinusLTestSamples/GenerateMadGraphConfig.py
+++ BMinusLTestSamples/GenerateMadGraphConfig.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python
 
+import sys
+import os.path
+import optparse
+import time
+
+import argparse
+
 def generateConfig(model_name, num_extra_jets=0):
     f = file('config_%s.sh' % model_name, "w")
 
@@ -12,6 +19,7 @@ def generateConfig(model_name, num_extra_jets=0):
     add_process_line = 'add process p p > t1 t1~'
     nj = 1
     while nj <= num_extra_jets:
+        print 'nj: %d -- %d' % (nj, num_extra_jets)
         add_process_line = '%s j' % add_process_line
         f.write("%s\n" % add_process_line)
         nj += 1
@@ -22,8 +30,42 @@ def generateConfig(model_name, num_extra_jets=0):
 
     f.close()
 
+def main():
+    print 'Generating MadGraph config file:'
+    parser = argparse.ArgumentParser(description='Generate MadGraph config file.')
+    parser.add_argument( '--model-dir', dest='model_dir', action='store'
+                       , default=None
+                       , help='model directory'
+                       )
+    parser.add_argument( '--stop-mass', dest='stop_mass', action='store'
+                       , default=None, type=int
+                       , help='stop mass'
+                       )
+    parser.add_argument( '--num-partons', dest='num_partons', action='store'
+                       , default=None, type=int
+                       , help='number additional partons'
+                       )
+    args = parser.parse_args()
 
-generateConfig('test_100_nj0', 0)
-generateConfig('test_100_nj1', 1)
-generateConfig('test_100_nj2', 2)
-generateConfig('test_100_nj3', 3)
+    model_dir = args.model_dir
+    stop_mass = args.stop_mass
+    num_partons = args.num_partons
+
+    if model_dir is None:
+        model_dir = raw_input("Model dir: ")
+    if stop_mass is None:
+        stop_mass = int(raw_input("Stop mass: "))
+    if num_partons is None:
+        num_partons = int(raw_input("Number additional partons: "))
+
+    model_name = '%s_%s_np%s' % (model_dir, stop_mass, num_partons)
+
+    print 'model_dir: %s' % model_dir
+    print 'stop_mass: %s' % stop_mass
+    print 'num_partons: %s' % num_partons
+    print 'model name: %s ' % model_name
+
+    generateConfig(model_name, num_partons)
+
+if __name__ == '__main__':
+    main()
--- BMinusLTestSamples/RunBMinusLTestSamples.sh
+++ BMinusLTestSamples/RunBMinusLTestSamples.sh
@@ -6,33 +6,49 @@
 # =   ../BMinusLTestSamples/RunBMinusLTestSamples.sh
 # ==============================================================================
 
+rel_path=$(echo $0 | sed "s#RunBMinusLTestSamples.sh##g")
+echo $rel_path
 model_dir="BMinusLTestSamples"
 
-for mass in 100 500 1000; do
-  model_name="${model_dir}_${mass}"
+# for mass in 100 500 1000; do
+for mass in 100 ; do
+  for njets in 0 1 ; do
+    echo ''
+    model_name="${model_dir}_${mass}_np${njets}"
+    param_card_name="${model_dir}_${mass}"
 
-  # generate diagrams and set up MG5 area
-  echo "Generating diagrams in MadGraph"
-  ./bin/mg5 ../${model_dir}/Configure${model_name}.sh
+    # generate madgraph config file
+    ./$rel_path/GenerateMadGraphConfig.py --model-dir=${model_dir} \
+                                          --stop-mass=${mass} \
+                                          --num-partons=${njets} 
 
-  # copy relevant files to Cards directory
-  echo "Copying files to Cards directory"
-  cp ../${model_dir}/${model_name}.slha    ${model_name}/Cards/param_card.dat
-  cp ../${model_dir}/pythia_card.dat       ${model_name}/Cards/pythia_card.dat
-  cp ../${model_dir}/run_card.dat          ${model_name}/Cards/run_card.dat
-  cp ../${model_dir}/me5_configuration.txt ${model_name}/Cards/me5_configuration.txt
+    # generate diagrams and set up MG5 area
+    echo "Generating diagrams in MadGraph"
+    ./bin/mg5 config_${model_name}.sh
 
-  sed -i "" "s#CONFIGURE_PATH#${PWD}#g" ${model_name}/Cards/me5_configuration.txt
+    # copy relevant files to Cards directory
+    echo "Copying files to Cards directory"
+    cp ./${rel_path}/${param_card_name}.slha ${model_name}/Cards/param_card.dat
+    cp ./${rel_path}/pythia_card.dat         ${model_name}/Cards/pythia_card.dat
+    if [[ $njets == "0" ]] ; then
+      cp ./${rel_path}/run_card_wojets.dat   ${model_name}/Cards/run_card.dat
+    else
+      cp ./${rel_path}/run_card_wjets.dat    ${model_name}/Cards/run_card.dat
+    fi
+    cp ./${rel_path}/me5_configuration.txt   ${model_name}/Cards/me5_configuration.txt
 
-  # move to model workspace
-  cd ${model_name}
+    sed -i "" "s#CONFIGURE_PATH#${PWD}#g" ${model_name}/Cards/me5_configuration.txt
 
-  # Generate events!
-  #   This will run just the matrix elemetn in MadGraph
-  ./bin/generate_events -f
-  # If you want to do both the matrix element (MadGraph) and the
-  #   decays/hadronization, use this command instead.
-  # ./bin/generate_events -f --laststep=pythia
+    # move to model workspace
+    cd ${model_name}
 
-  cd ..
+    # Generate events!
+    #   This will run just the matrix elemetn in MadGraph
+    ./bin/generate_events -f
+    # If you want to do both the matrix element (MadGraph) and the
+    #   decays/hadronization, use this command instead.
+    # ./bin/generate_events -f --laststep=pythia
+
+    cd ..
+  done
 done
