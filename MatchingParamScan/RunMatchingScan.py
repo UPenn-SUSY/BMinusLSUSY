43277b5805161e949d87f2f235ca82d4c8d0f02c
bdjackson <bjack3@gmail.com>
2013-12-16 16:36:25 +0100

    add script to run scan

---
+++ MatchingParamScan/RunMatchingScan.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+mass_min = 100
+mass_max = 1000
+mass_step = 100
+
+xqcut_min_factor = 10.
+xqcut_max_factor = 2.
+xqcut_step = 10
+
+qcut_step = 10
+
+counter = 0
+for mass in xrange(mass_min, mass_max + mass_step, mass_step):
+    xqcut_min = int(mass/xqcut_min_factor)
+    xqcut_max = int(mass/xqcut_max_factor)
+    print 'mass: %s  xqcut min: %s  xqcut max: %s' % (mass, xqcut_min, xqcut_max)
+    for xqcut in xrange(xqcut_min, xqcut_max + xqcut_step, xqcut_step):
+        qcut_min = xqcut + qcut_step
+        qcut_max = xqcut*2
+        for qcut in xrange(qcut_min, qcut_max + qcut_step, qcut_step):
+            counter += 1
+
+print counter
