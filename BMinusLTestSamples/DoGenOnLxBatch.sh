7244f49103c392fc6cc265b9a224ac15fc3d8aed
bdjackson <bjack3@gmail.com>
2015-07-16 02:13:13 +0200

    update script for new release

--- BMinusLTestSamples/DoGenOnLxBatch.sh
+++ BMinusLTestSamples/DoGenOnLxBatch.sh
@@ -65,6 +65,9 @@ if [[ ! -d truth_d3pd ]]; then
   mkdir truth_d3pd
 fi
 
+# make config file name
+config_file_name="MC15.${dsid}.MGPy8EG_${short_name}.py"
+
 # make job option file
 jo_file_name="jobs/jo.${dsid}.${short_name}.sh"
 echo "#!/bin/bash" > $jo_file_name
@@ -75,18 +78,9 @@ echo "# setup environment and athena" >> $jo_file_name
 echo 'LOCAL_WORK_DIR=${PWD}' >> $jo_file_name
 echo "source ~/.bash_profile" >> $jo_file_name
 # echo "cd ${TestArea}" >> $jo_file_name
-echo "source ${AtlasSetup}/scripts/asetup.sh 20.1.4.14,slc6,gcc48" >> $jo_file_name
-# echo 'export JOBOPTSEARCHPATH=/cvmfs/atlas.cern.ch/repo/sw/Generators/MC12JobOptions/latest/common:$JOBOPTSEARCHPATH' >> $jo_file_name
-# echo 'export JOBOPTSEARCHPATH=/cvmfs/atlas.cern.ch/repo/sw/Generators/MC12JobOptions/latest/susycontrol:$JOBOPTSEARCHPATH' >> $jo_file_name
+echo "source ${AtlasSetup}/scripts/asetup.sh 19.2.4.4.2,MCProd,slc6,gcc47" >> $jo_file_name
 echo 'cd ${LOCAL_WORK_DIR}' >> $jo_file_name
 echo "" >> $jo_file_name
-
-# # Copy MadGraph to local directory
-# echo "# copy MadGraph to local directory" >> $jo_file_name
-# # make this more robust to pwd when calling script
-# echo "cp -r ${PWD}/../MadGraph5_v1_5_12 ." >> $jo_file_name
-# echo 'MADPATH=${PWD}/MadGraph5_v1_5_12' >> $jo_file_name
-# echo "" >> $jo_file_name
 
 # make directory for this sample
 local_dir_name="${dsid}.${short_name}.com_e_${com_energy}"
@@ -108,44 +102,42 @@ echo "" >> $jo_file_name
 
 # pick correct MadGraphControl script
 echo "# picking correct MadGraphControl script" >> $jo_file_name
-if [[ "$decay_stops_in_mg" == "1" ]] ; then
-  if [[ "$num_additional_partons" == "1" ]] ; then
-    echo "cp MadGraphControl_SM_TT_directBL.mg_stop__mg_decay.py MadGraphControl_SM_TT_directBL.py" >> $jo_file_name
-  elif [[ "$num_additional_partons" == "0" ]] ; then
-    echo "cp MadGraphControl_SM_TT_directBL.np0.mg_stop__mg_decay.py MadGraphControl_SM_TT_directBL.py" >> $jo_file_name
-  fi
-else
-  if [[ "$num_additional_partons" == "1" ]] ; then
-    echo "cp MadGraphControl_SM_TT_directBL.mg_stop__pythia_decay.py MadGraphControl_SM_TT_directBL.py" >> $jo_file_name
-  elif [[ "$num_additional_partons" == "0" ]] ; then
-    echo "cp MadGraphControl_SM_TT_directBL.np0.mg_stop__pythia_decay.py MadGraphControl_SM_TT_directBL.py" >> $jo_file_name
-  fi
-fi
+echo "echo \
+\"include('MadGraphControl_SM_TT_directBL.mg_stop__pythia_decay.py')\" \
+> ${config_file_name}" >> $jo_file_name
 echo "" >> $jo_file_name
 
-# Run generate trf
-echo "# run generate trf" >> $jo_file_name
-## echo "Generate_trf.py runNumber=${dsid} randomSeed=1 firstEvent=1 jobConfig=MadGraphControl_SM_TT_directBL.py outputEVNTFile=${pool_file_name} ecmEnergy=8000 maxEvents=${num_events}" >> $jo_file_name
-# echo "Generate_trf.py runNumber=${dsid} randomSeed=1 firstEvent=1 jobConfig=MadGraphControl_SM_TT_directBL.py outputEVNTFile=${pool_file_name} ecmEnergy=${com_energy} maxEvents=${num_events}" >> $jo_file_name
+# Run generate tf
+echo "# run generate tf" >> $jo_file_name
 echo "Generate_tf.py \
 --runNumber=${dsid} \
 --randomSeed=1 \
 --firstEvent=1 \
---jobConfig=MadGraphControl_SM_TT_directBL.py \
+--jobConfig=${config_file_name} \
 --outputEVNTFile=${pool_file_name} \
 --ecmEnergy=${com_energy} \
 --maxEvents=${num_events}" \
 >> $jo_file_name
 echo "" >> $jo_file_name
 
-# Run reco trf
-echo "# run reco trf" >> $jo_file_name
-echo "Reco_trf.py inputEVNTFile=${pool_file_name} outputNTUP_TRUTHFile=${truth_ntuple_name}" >> $jo_file_name
+# change release for reco tf
+echo "source \
+/afs/cern.ch/atlas/software/dist/AtlasSetup/scripts/asetup.sh 20.1.5.4,AtlasDerivation,slc6,gcc47" \
+>> $jo_file_name
+echo "" >> $jo_file_name
+
+# Run reco tf
+echo "# run reco tf" >> $jo_file_name
+# echo "Reco_trf.py inputEVNTFile=${pool_file_name} \
+# outputNTUP_TRUTHFile=${truth_ntuple_name}" >> $jo_file_name
+echo "Reco_tf.py --inputEVNTFile ${pool_file_name} \
+--outputDAODFile ${truth_ntuple_name} \
+--reductionConf TRUTH1" >> $jo_file_name
 echo "ls" >> $jo_file_name
 echo "" >> $jo_file_name
 
 # Clean up big files before copying
-echo "rm madgraph.202632.madgraph_SM_TT_directBL_100._00001.events" >> $jo_file_name
+echo "rm madgraph.*.madgraph_SM_TT_directBL_*._00001.events" >> $jo_file_name
 echo "rm events.lhe" >> $jo_file_name
 echo "rm evgen.*.pool.root" >> $jo_file_name
 echo "rm -rf PROC_ReducedUFO_0" >> $jo_file_name
@@ -155,7 +147,8 @@ echo "" >> $jo_file_name
 echo "# Copy dir to afs" >> $jo_file_name
 echo "cd .." >> $jo_file_name
 echo "cp -r ${local_dir_name} ${dir_on_afs_work}/samples" >> $jo_file_name
-echo "mv ${dir_on_afs_work}/samples/${local_dir_name}/${truth_ntuple_name} ${dir_on_afs_work}/truth_d3pd/${truth_ntuple_name}" >> $jo_file_name
+echo "mv ${dir_on_afs_work}/samples/${local_dir_name}/${truth_ntuple_name} \
+${dir_on_afs_work}/truth_d3pd/${truth_ntuple_name}" >> $jo_file_name
 echo "" >> $jo_file_name
 
 # submit job to batch
--- BMinusLTestSamples/configs/MadGraphControl_SM_TT_directBL.mg_stop__pythia_decay.py
+++ BMinusLTestSamples/configs/MadGraphControl_SM_TT_directBL.mg_stop__pythia_decay.py
@@ -1,7 +1,4 @@
-from MadGraphControl.MadGraphUtils import *
-# ==============================================================================
-# = stop pair-production with stop > b l
-# ==============================================================================
+include ( 'MC15JobOptions/MadGraphControl_SimplifiedModelPreInclude.py' )
 
 # ------------------------------------------------------------------------------
 # basic config stuff
@@ -9,242 +6,37 @@ print '-------------------------------------------------------------------------
 print 'Setting basic config stuff'
 print '--------------------------------------------------------------------------------'
 therun = runArgs.runNumber-202632
-if therun < 0 and therun >= 20:
+if therun < 0 and therun >= 50:
     evgenLog.fatal('Invalid run number! Will fail now!')
     raise RuntimeError('Unknown run number')
-
-masses = {}
 masses['1000006'] = 50*therun+100 # 50 GeV scan over stop masses, 20 points
-stringy = str(masses['1000006']) # Goes into the file name
-gentype='TT' # stop-stop
+
+gentype = 'TT'
 decaytype='directBL' # your magic word
-njets=1 # Number of jets in MadGraph
-evgenLog.info('Registered generation of stop pair production, stop to bl; grid point '+str(therun)+' decoded into mass ' + str(masses['1000006']) + ', with ' + str(njets) + ' jets.')
-use_decays=False # To turn on decays in MadGraph
-evt_multiplier = 20.0
+njets=0
 
-evgenConfig.contact  = [ "bjackson@cern.ch" ]
-evgenConfig.keywords += ['stop']
-evgenConfig.description = 'stop direct pair production with simplified model'
-evgenConfig.minevents=50000
-
-# ------------------------------------------------------------------------------
-# Basic settings for production and filters
-print '--------------------------------------------------------------------------------'
-print 'Setting basic settings'
-print '--------------------------------------------------------------------------------'
-SLHAonly = False
-syst_mod=None
-use_Tauola=True
-use_METfilter=False
-use_MultiElecMuTauFilter=False
-use_MultiLeptonFilter=False
-
-# ------------------------------------------------------------------------------
-# Set up skip events - should almost never be used
-skip_events=0
-
-# ------------------------------------------------------------------------------
-# Set random seed
-rand_seed=1234
-if hasattr(runArgs, "randomSeed"):
-    rand_seed=runArgs.randomSeed
-
-# ------------------------------------------------------------------------------
-# Set beam energy - default to 8 TeV c.o.m.
-beamEnergy = 4000.
-if hasattr(runArgs,'ecmEnergy'):
-    beamEnergy = runArgs.ecmEnergy / 2.
-
-# ------------------------------------------------------------------------------
-if 'EventMultiplier' in dir(): evt_multiplier=EventMultiplier
-if hasattr(runArgs,'EventMultiplier'): evt_multiplier=runArgs.EventMultiplier
-nevts=5000*evt_multiplier
-evt_multiplier=-1
-
-# ------------------------------------------------------------------------------
-# Set pythia6 or pythia8
-usePythia6 = True
-
-# ------------------------------------------------------------------------------
-# Default matching parameters
-print '--------------------------------------------------------------------------------'
-print 'Finding matching parameters'
-print '--------------------------------------------------------------------------------'
-ickkw = 1
-xqcut = masses['1000006']/4
-qcut = xqcut
-# xqcut = masses['1000006']/6
-# while not xqcut%5 == 0:
-#     xqcut -= 1
-# qcut = 2*xqcut
-print 'xqcut: %s' % xqcut
-print 'qcut: %s' % qcut
-
-# ------------------------------------------------------------------------------
-# Write proc_card
-print '--------------------------------------------------------------------------------'
-print 'Writing proc_card'
-print '--------------------------------------------------------------------------------'
-proc_card_out = open('proc_card_mg5.dat','w')
-proc_card_out.write("""
-import model mssm
-# Define multiparticle labels
-define p = g u c d s u~ c~ d~ s~
-define j = g u c d s u~ c~ d~ s~
-define l+ = e+ mu+ ta+
-define l- = e- mu- ta-
-define vl = ve vm vt
-# define vl~ = ve~ vm~ vt~
-# define fu = u c e+ mu+ ta+
-# define fu~ = u~ c~ e- mu- ta-
-# define fd = d s ve~ vm~ vt~
-# define fd~ = d~ s~ ve vm vt
-
-# Specify process(es) to run
-
-""")
-
+process = ''
 starter = 'generate'
-for jet_it in xrange(njets + 1):
+for jet_it in xrange(njets+1):
     jet_string = ''
     for i in xrange(jet_it):
         jet_string += 'j '
 
-    proc_string = '%s p p > t1 t1~ %s $ go ul ur dl dr cl cr sl sr t2 b1 b2 ul~ ur~ dl~ dr~ cl~ cr~ sl~ sr~ b1~ t2~ b2~ @%d\n '% (starter, jet_string, jet_it)
-    # proc_string = '%s p p > t1 t1~ %s , t1 > b l+ , t1~ > b~ l- $ go ul ur dl dr cl cr sl sr t2 b1 b2 ul~ ur~ dl~ dr~ cl~ cr~ sl~ sr~ b1~ t2~ b2~ @%d\n '% (starter, jet_string, jet_it)
-
-    proc_card_out.write(proc_string)
+    proc_string = '%s p p > t1 t1~ %s $ go susylq susylq~ b1 b2 t2 b1~ b2~ t2~ @%d' % (starter, jet_string, jet_it)
+    process = '\n'.join([process, proc_string])
+    # process = '%s%s\n' % (process, proc_string)
 
     starter = 'add process'
 
-proc_card_out.write("""
+# evt_multiplier = 20.0
+evt_multiplier = 1.0
+evgenLog.info('Registered generation of stop pair production, stop to bl; grid point '+str(therun)+' decoded into mass ' + str(masses['1000006']) + ', with ' + str(njets) + ' jets.')
 
-# Output processes to MadEvent directory
-output -f
-""")
-proc_card_out.close()
-print 'Proc card is written and closed!'
+evgenConfig.contact  = [ "bjackson@cern.ch" ]
+evgenConfig.keywords += ['stop']
+evgenConfig.description = 'stop direct pair production with simplified model'
 
-# ------------------------------------------------------------------------------
-# write run card
-print '--------------------------------------------------------------------------------'
-print 'Writing run_card'
-print '--------------------------------------------------------------------------------'
-alpsfact = 1.0
-scalefact=1.0
-build_run_card( run_card_old='run_card.SM.dat'
-              , run_card_new='run_card.dat'
-              , xqcut=xqcut
-              , nevts=nevts
-              , rand_seed=rand_seed
-              , beamEnergy=beamEnergy
-              , scalefact=scalefact
-              , alpsfact=alpsfact
-              )
-print 'Done writing run_card'
+include ( 'MC15JobOptions/MadGraphControl_SimplifiedModelPostInclude.py' )
 
-# ------------------------------------------------------------------------------
-# Write param_card
-print '--------------------------------------------------------------------------------'
-print 'Writing param_card'
-print '--------------------------------------------------------------------------------'
-offshellness = ''
-decays = {}
-dummy_param_card_name = 'param_card.SM.%s.%s.%sdat' % ( gentype
-                                                      , decaytype
-                                                      , offshellness
-                                                      )
-build_param_card( param_card_old=dummy_param_card_name
-                , param_card_new='param_card.dat'
-                , masses=masses
-                , decays=decays
-                )
-print 'Done writing param_card'
-print 'dummy param card: %s' % dummy_param_card_name
-print 'new param_card: param_card.dat'
-
-# ------------------------------------------------------------------------------
-# generate process
-print '--------------------------------------------------------------------------------'
-print 'Creating new process'
-print '--------------------------------------------------------------------------------'
-process_dir = new_process()
-# process_dir = 'PROC_mssm_0'
-print 'prcess_dir: %s' % process_dir
-# proc_card_out = open('proc_card_mg5.dat','w')
-
-print 'Generating process'
-generate( run_card_loc = 'run_card.dat'
-        # , param_card_loc = None
-        , param_card_loc = 'param_card.dat'
-        , mode = 0
-        , njobs = 1
-        , run_name = 'Test'
-        , proc_dir = process_dir
-        )
-print 'done generating process'
-
-# if hasattr(runArgs,'skipEvents'):
-#     skip_events = runArgs.skipEvents
-# arrange_output( run_name = 'Test'
-#               , proc_dir = process_dir
-#               , outputDS = stringy + '._00001.events.tar.gz'
-#               , skip_events = skip_events
-#               )
-
-# Move output files into the appropriate place, with the appropriate name
-run_number = runArgs.runNumber
-outputDS = 'madgraph.%i.madgraph_SM_%s_%s_%s._00001.events.tar.gz'%( run_number
-                                                                   , gentype
-                                                                   , decaytype
-                                                                   , stringy
-                                                                   )
-print '--------------------------------------------------------------------------------'
-print 'About to arrange output'
-print '--------------------------------------------------------------------------------'
-if arrange_output( run_name='Test'
-                 , proc_dir=process_dir
-                 , outputDS=outputDS
-                 , skip_events=skip_events
-                 ):
-    mglog.error('Error arranging output dataset!')
-    # return -1
-    raise RuntimeError('Error arranging output dataset!')
-
-# if not keepOutput:
-#     mglog.info('Removing process directory...')
-#     shutil.rmtree(thedir,ignore_errors=True)
-
-# mglog.info('All done generating events!!')
-# mglog.info('returning 2*xqcut as qcut value: %s' % 2*xqcut)
-# return [2*xqcut,'madgraph.%i.madgraph_SM_%s_%s_%s._00001.events.tar.gz'%(run_number,gentype,decaytype,stringy)]
-
-
-# ------------------------------------------------------------------------------
-# Run pythia
-print '--------------------------------------------------------------------------------'
-print 'About to run pythia'
-print '--------------------------------------------------------------------------------'
-runArgs.qcut = qcut
-runArgs.inputGeneratorFile = outputDS
-if 'syst_mod' in dir():
-    runArgs.syst_mod = syst_mod
-runArgs.decaytype = decaytype
-runArgs.gentype = gentype
-runArgs.use_Tauola = use_Tauola
-runArgs.use_METfilter = use_METfilter
-runArgs.use_MultiElecMuTauFilter = use_MultiElecMuTauFilter
-runArgs.use_MultiLeptonFilter = use_MultiLeptonFilter
-
-if usePythia6:
-    print 'running pythia6'
-    evgenLog.info('Will use Pythia6...')
-    include( 'MC12JobOptions/MadGraphControl_SimplifiedModelPythia6.py' )
-else:
-    print 'running pythia8'
-    evgenLog.info('Will use Pythia8...')
-    if 'mmjj' in dir(): runArgs.mmjj=mmjj
-    if 'drjj' in dir(): runArgs.drjj=drjj
-    if 'ptj'  in dir(): runArgs.ptj =ptj
-    include( 'MC12JobOptions/MadGraphControl_SimplifiedModelPythia8.py' )
+if njets>0:
+    genSeq.Pythia8.Commands += ["Merging:Process = pp>{t1,1000006}{t1~,-1000006}"]
